ecc7c74bc3587ea7947f6a0a2664c688e5755734
Sem-Phanun <phanunsem097@gmai.com>
2024-02-15 15:24:51 +0700

    modify auth controller route

--- controller/authController.js
+++ controller/authController.js
@@ -27,7 +27,7 @@ export const register = async (req, res) => {
 
     //checking existing user
     if (existingUser) {
-      return res.status(200).send({
+      return res.status(201).send({
         success: false,
         message: "User already register please login",
       });
@@ -43,7 +43,7 @@ export const register = async (req, res) => {
 
     await user.save();
 
-    return res.status(200).json({
+    return res.status(201).json({
       success: true,
       message: "register sccuess",
       user,
@@ -101,3 +101,93 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.params })
+    return res.status(200).json({
+      success: true,
+      message: "get single user success",
+      user,
+  })
+  } catch (error) {
+    return res.status(500).send({
+      error: error,
+      success: true,
+      message: "get user failed"
+    })
+  }
+}
+
+export const changePassword = async (req, res) => {
+  try {
+    const {email, newPassword } = req.body
+    if (!email) {
+      return res.status(400).send({
+        message: "email is required"
+      })
+    }
+    if (!newPassword) {
+      return res.status(400).send({
+        message: "new password is required"
+      })
+    }
+
+    //check user
+    const user = await userModel.findOne({email})
+
+    //validate
+    if (!user) {
+      return res.status(400).send({
+        success: false,
+        message: "wrong email"
+      })
+    }
+    const hashed = await hashPassword(newPassword)
+    await userModel.findByIdAndUpdate(user._id, {password: hashed})
+    return res.status(201).send({
+      success: true,
+      message: "password reset successfully"
+    })
+  } catch (error) {
+    return res.status(500).send({
+      error: error,
+      success: false,
+      message: "something went wrong"
+    })
+  }
+}
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    const user = await userModel.findById(req.user._id)
+
+    //password must be more than 6 charactor
+    if (password && password.length < 6) {
+      return res.json({ message: "password must be more than 6 charactor"})
+    }
+    const hashedPassword = password ? await hashPassword(password) : undefined
+
+    //update 
+    const updateUser = await userModel.findByIdAndUpdate(req.user._id,
+      {
+        username: username || user.username,
+        password: password || user.password
+      },
+      {new: true}
+    )
+    res.status(201).send({
+      success: true,
+      message: "Profile Updated SUccessfully",
+      updateUser,
+    });
+
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      error: error,
+      message: "Profile update failed"
+    })
+  }
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { login, register } from '../controller/authController.js'
+import { changePassword, login, register, updateUserProfile } from '../controller/authController.js'
 import { isAdmin, requireSign } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post("/register",register)
-router.post("/login", login)
+router.post("/login", requireSign,isAdmin, login)
+router.post("/forget-password", requireSign,isAdmin, changePassword)
+router.put("/update-profile", requireSign, isAdmin, updateUserProfile)
 
 export default router
