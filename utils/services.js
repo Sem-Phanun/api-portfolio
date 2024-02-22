f32c86b5fc9b7c15cfa39196f1dd2ea7b767a370
Sem-Phanun <phanunsem097@gmai.com>
2024-02-23 01:30:04 +0700

    refactoring

--- utils/services.js
+++ utils/services.js
@@ -1,25 +1,20 @@
 import express from 'express'
 import multer from 'multer'
 import path from 'path'
-import { fileURLToPath } from 'url'
+
 
 
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-
-app.use("/", express.static(path.join(__dirname, "public/upload")))
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
       cb(null, "public/upload")
     },
     filename: function(req, file, cb){
-      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()* 1E9)
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()* 1E9) + path.extname(file.originalname) 
       req.body.image = uniqueSuffix
-      cb(null, "img-"+ uniqueSuffix + path.extname(file.originalname))  
+      cb(null, uniqueSuffix)  
     }
   })
   
